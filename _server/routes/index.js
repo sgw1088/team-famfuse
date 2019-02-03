93d51f3658edf40718b9c1e3398aa9b0a8b98350
Andrew Studley <studleyad2@gmail.com>
2019-02-03 16:28:38 -0700

    add _server file for back-end

---
+++ _server/routes/index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../models');
+const auth = require("../config/auth");
+var multer = require('multer');
+const storage = multer.diskStorage({
+  destination: function(req,file,cb) {
+    cb(null,'uploads/');
+  },
+  filename: function(req,file,cb) {
+    cb(null, file.originalname);
+  }
+})
+var upload = multer({ storage: storage})
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'FamFuse' });
+});
+
+router.get('/feed', auth.verifyUser, function(req, res, next) {
+  models.feeds
+  .findAll({
+    where: {
+      familyCode: req.user.familyCode
+    }
+  }).then(familyFeed => {
+    res.render('feed', {
+      feed: familyFeed
+    });
+  })
+})
+router.post('/newimage', upload.single('image'), function(req,res,next) {
+  console.log(req.image)
+})
+router.post('/newpost', auth.verifyUser, function(req, res, next){
+  console.log(req.body.file);
+  models.feeds
+  .create({
+    userId: req.user.userId,
+    message: req.body.message,
+    firstName: req.user.firstName,
+    lastName: req.user.lastName,
+    familyCode: req.user.familyCode,
+    username: req.user.username
+  }).then(post => {
+      res.redirect('feed'); 
+  })
+})
+
+module.exports = router;
---
+++ _server/routes/users.js
@@ -0,0 +1,156 @@
+var express = require('express');
+var router = express.Router();
+
+const sqlite = require('sqlite3').verbose();
+var models = require('../models');
+
+const auth = require("../config/auth");
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+  models.users
+  .findAll({})
+  .then(allUsers => {
+    res.send(JSON.stringify(allUsers));
+  })
+});
+router.get('/login', function(req,res,next) {
+  res.render('login');
+})
+router.get('/register',function(req,res,next) {
+  res.render('register');
+})
+
+router.post('/register',function(req,res,next) {
+  const hashedPassword = auth.hashPassword(req.body.password);
+  models.users
+    .findOne({
+      where: {
+        Username: req.body.username
+      }
+    })
+    .then(user => {
+      if (user) {
+        res.send('this user already exists');
+      } else {
+        const code = Math.random().toString(36).substr(2, 9) + req.body.lastName;
+        if(req.body.usertype == 'parent' && req.body.createfamily == 'true') {
+          models.users
+          .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userType: req.body.usertype,
+            email: req.body.email,
+            password: hashedPassword,
+            familyCode: code,
+            username: req.body.email
+          })
+          .then(createdUser => {
+            const isMatch = createdUser.comparePassword(req.body.password);
+            if (isMatch) {
+              const userId = createdUser.userId;
+              console.log(userId);
+              const token = auth.signUser(createdUser);
+              res.cookie('jwt', token);
+              res.send(JSON.stringify(createdUser));
+            } else {
+              console.error('not a match');
+            }
+          });
+        } else {
+          models.users
+          .create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userType: req.body.usertype,
+            email: req.body.email,
+            password: hashedPassword,
+            familyCode: req.body.familycode,
+            username: req.body.email
+          })
+          .then(createdUser => {
+            const isMatch = createdUser.comparePassword(req.body.password);
+            if (isMatch) {
+              const userId = createdUser.userId;
+              console.log(userId);
+              const token = auth.signUser(createdUser);
+              res.cookie('jwt', token);
+              res.send(JSON.stringify(createdUser));
+            } else {
+              console.error('not a match');
+            }
+          });
+        }
+      }
+    });
+});
+      
+
+
+
+router.post('/login', function (req, res, next) {
+  const hashedPassword = auth.hashPassword(req.body.password);
+  models.users.findOne({
+    where: {
+      email: req.body.username
+    }
+  }).then(user => {
+    const isMatch = user.comparePassword(req.body.password)
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Login Failed"
+      });
+    }
+    if (isMatch) {
+      const userId = user.userId
+      const token = auth.signUser(user);
+      res.cookie('jwt', token);
+      res.send(JSON.stringify(user));
+      res.render('/profile/' + userId)
+    } else {
+      console.log(req.body.password);
+      res.redirect('/users/login')
+    }
+
+  });
+});
+
+router.get('/profile/:id', auth.verifyUser, function (req, res, next) {
+  if (req.params.id !== String(req.user.userId)) {
+    res.send('This is not your profile')
+  } else {
+    res.render('profile', {
+      FirstName: req.user.firstName,
+      LastName: req.user.lastName,
+      Email: req.user.email,
+      UserId: req.user.userId,
+      familyCode: req.user.familyCode
+    })
+  } 
+});
+
+router.get('/myfamily/:id', auth.verifyUser, function(req,res,next){
+  if (req.params.id !== req.user.familyCode) {
+    res.send('Please login to see this page.')
+  } else {
+  models.users
+    .findAll({
+      where: {
+        familyCode: req.params.id
+      }
+    }).then(allFamily => {
+      res.render('myfamily', {
+        family: allFamily
+      })
+    })
+  }
+})
+
+router.get('/logout', function (req, res) {
+  res.cookie('jwt', null);
+  res.redirect('/users/login');
+});
+
+module.exports = router;
