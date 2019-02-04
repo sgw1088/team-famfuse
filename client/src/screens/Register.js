fc7f1ce621e969bccaea86e2280cb7af2b7cd849
Andrew Studley <studleyad2@gmail.com>
2019-02-03 17:11:04 -0700

    Add reg to front-end

--- client/src/screens/Register.js
+++ client/src/screens/Register.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Register = () => (
   <div>
-    <h2>Register</h2>
+    <div>
+        <form id="signup" name="signup" method="POST" action="/users/register">
+          <div>
+              <label >First Name: </label>
+              <input type="text" name="firstName" required></input>
+          </div>
+          <div>
+              <label >Last Name: </label>
+              <input type="text" name="lastName" required></input>
+          </div>
+          <div>
+              <label >Email: </label>
+              <input type="text" name="email" required></input>
+          </div>
+          <div>
+            <label>Parent</label>
+            <input type="radio" name="usertype" value="parent"></input><br /> 
+            <label>Child</label>
+            <input type="radio" name="usertype" value="child"></input> 
+            
+          </div>
+          <div>
+            <label>Create New Family: </label>
+            <input type="checkbox" name="createfamily" value="true"></input>
+          </div>
+          <div>
+            <label>Family Code: </label>
+            <input type="text" name="familycode"></input>
+          </div>
+          <div>
+              <label >Password: </label>
+              <input type="text" name="password" required></input>
+          </div>
+          <div>
+              <button type="submit">Submit</button>
+          </div>
+        </form>
+      </div>
   </div>
   
 );
